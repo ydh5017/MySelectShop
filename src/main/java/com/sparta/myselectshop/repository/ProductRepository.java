2d31394b4502d410d6fb75f4ee8b3dbbad9a4e6a
ydh5017 <ydh5017@naver.com>
2024-05-23 12:59:15 +0900

    주석 추가

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -12,5 +12,20 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findAllByUser(User user, Pageable pageable);
 
+    /**
+     * <p>
+     *     select *
+     *     from product p left join product_folder pf on p.id = pf.product_id
+     *     where p.user_id = #{user.getId}
+     *      and pf.folder_id = #{folderId}
+     *     order by p.id #{pageable.?}
+     *     limit #{pageable.?}, #{pageable.?}
+     *
+     * </p>
+     * @param user 회원정보
+     * @param folderId 폴더 ID
+     * @param pageable 페이징
+     * @return 페이징 된 상품 목록
+     */
     Page<Product> findAllByUserAndProductFolderList_FolderId(User user, Long folderId, Pageable pageable);
 }
